aa2882699bd77b912ddecbf9df993a79e2771a94
InSeo <baginseo400@gmail.com>
2024-01-19 17:51:39 +0900

    20240119

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -1,19 +1,25 @@
-const boxs=document.querySelectorAll(".box");
-const inputs=document.querySelectorAll(".input");
-const btn=document.querySelector(".btn");
+
+//요소를 한 번에 여러개 얻어오면 배열 형태로 저장된다!!
+// -> 얻어온 요소를 사용하려면 배열의 index별 요소를 하나씩 꺼내서 
+//    사용해야 한다!!!
+
+// ex)boxList.style.colore="red"   => 글자색 변경 X
+//    boxList[0].style.colore="red" => 글자색 변경 O
+
+const boxList=document.querySelectorAll(".box"); //배열
+const inputList=document.querySelectorAll(".input");
 
 
-btn.addEventListener("click",function(){
+document.querySelector(".btn").addEventListener("click",function(){
 
-    for(let idx=0; idx<boxs.length; idx++){
-
-        boxs[idx].style.backgroundColor=inputs[idx].value;
-
-        if(inputs[idx].value==""){
+    for(let idx=0; idx<boxList.length; idx++){
+        boxList[idx].style.backgroundColor=inputList[idx].value;
+        
+        if(inputList[idx].value==""){
             alert(`${idx}번째 input란이 비어있음`);
         }
     }
 
-    inputs[0].focus();
+    inputList[0].focus();
 });
 
